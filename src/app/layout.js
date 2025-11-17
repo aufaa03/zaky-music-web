cb87004bfe4bc399f3a3a1ffa177cf0710546ee9
aufaa03 <142073902+aufaa03@users.noreply.github.com>
2025-11-18 00:10:00 +0700

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 const geistSans = Geist({
@@ -52,6 +53,7 @@ export default function RootLayout({ children }) {
 
         {/* Nanti Footer taruh sini */}
         <Footer/>
+        <SpeedInsights />
       </body>
     </html>
   );
